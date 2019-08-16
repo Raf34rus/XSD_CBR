85ffb31a322fdc9058c377c98d79059cdd5d3492
Rafael Muleev <hfa@list.ru>
2019-08-16 13:42:26 +0400

    start

--- XSD_CBR/Program.cs
+++ XSD_CBR/Program.cs
@@ -16,7 +16,7 @@ namespace XSD_CBR
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Main());
         }
     }
 }
